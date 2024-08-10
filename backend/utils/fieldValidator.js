de1f76b7b7d83f50b2c686ce7c57d37b5215f8ca
aman <khanamankhan1973@gmail.com>
2024-08-10 09:01:27 +0530

    small fix

--- backend/utils/fieldValidator.js
+++ backend/utils/fieldValidator.js
@@ -7,10 +7,10 @@ export default function fieldValidator(fields){
     for(const key in fields){
         if(fields.hasOwnProperty(key)){
             if(fields[key] === undefined){
-                return {error: true, message: 'Bad Request: Request error.'}
+                return {error: true, message: `Bad Request: ${key} is required.`}
             }
             if(typeof fields[key] === 'string' && fields[key] === ''){
-                return {error: true, message: 'Bad Request: Request error.'}
+                return {error: true, message: `Bad Request: ${key} is required.`}
             }
         }
     }
